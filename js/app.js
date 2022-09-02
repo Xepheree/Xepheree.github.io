2c92c46336236faf3006772061702753c2b7c16a
Xepheree <59102398+Xepheree@users.noreply.github.com>
2022-09-03 00:31:55 +0800

    Sir Fiel Delivery Unit Cluster

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+//Hero Slider
+$('#hero-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    smartSpeed:1000,
+    items:1
+})
+});
